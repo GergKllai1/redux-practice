fe92bb6e367fbb15f8b02fe1a07967b84d2ff913
Gregory Kallai <gerg.kallai@gmail.com>
2019-05-26 11:12:46 +0200

    Adds a middleware that logs state and actions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import personReducer from "./reducers/personReducer";
 import { Provider } from "react-redux";
 import { persistStore, persistReducer } from "redux-persist";
@@ -15,13 +15,28 @@ const persistConfig = {
   storage: storage
 };
 
+const logger = state => {
+  return next => {
+    return action => {
+      console.log("Previous state:");
+      console.log(state.getState());
+      console.log("Dispatching action:");
+      console.log(action);
+      const result = next(action);
+      console.log("Next state:");
+      console.log(state.getState());
+      return result;
+    };
+  };
+};
+
 const rootReducer = combineReducers({
   ppl: personReducer
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(persistedReducer);
+const store = createStore(persistedReducer, applyMiddleware(logger));
 let persistor = persistStore(store);
 
 ReactDOM.render(
